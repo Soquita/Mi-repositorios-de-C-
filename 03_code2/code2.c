0ae2a1cd334482f14f8d84213c92ee3c5a23a7df
Soquita <48394713+Soquita@users.noreply.github.com>
2019-07-06 14:30:49 -0500

    Add files via upload

---
+++ 03_code2/code2.c
@@ -0,0 +1,22 @@
+int printTriangle(int size){
+  int starcount = 0;
+  for (int i=0;i<size;i++){
+    for (int j=0;j<=i;j++){
+      printf("*");
+      starcount=starcount+1;
+    }
+      printf("\n");
+  }
+  return starcount;
+}  
+int main(void){
+  int numStars;
+  printf("Here is a triangle with height 4\n");
+  numStars = printTriangle(4);
+  printf("That triangle had %d total stars\n", numStars);
+  printf("Here is a triangle with height 7\n");
+  numStars = printTriangle(7);
+  printf("That triangle had %d total stars\n", numStars);
+  return 0;
+}
+
